c0b1c519a1511e0bebc30b9d18dbac7e09588a37
Agustin-Fernandez <agustin.fernandez.tdlzal@gmail.com>
2019-09-12 00:21:07 -0300

    Ups, no comiteo hace bocha

--- listaEnlazada.s
+++ listaEnlazada.s
@@ -5,37 +5,49 @@
 # En esta implementación, 0 representa null para las direcciones.
 
 .data
-lista: .word 0					# Direccion del primer elemento. Se inicializa en 0 cuando no hay elementos.
-OFFSET_DIRECCION = 0			# Offset desde la direccion de un elemento y la direccion del siguiente
-OFFSET_DATO	= 32				# Offset desde la direccion de un elemento y su dato almacenado
-elemTest: .asciiz "Hola capo"
-elemTest2: .asciiz "Que onda man"
+OFFSET_DIRECCION = 0			# Offset desde la direccion de un elemento y la direccion del siguiente.
+OFFSET_DATO	= 4					# Offset desde la direccion de un elemento y su dato almacenado.
+STRING_MAX = 4					# Máxima cantidad de caracteres para los strings entrados con la funcion string.
+
+#lista: .word 0					# Direccion del primer elemento. Se inicializa en 0 cuando no hay elementos.
+
 
 .text
+  
+# Pide un string por consola.
+# No toma inputs.
+#########################################
+string:									#
+  li $t0, 2								# Cada caracter ocupa dos bits.
+  mul $t1, $t0, STRING_MAX				# $t1 - Cantidad de bits maximos para el string.
+										#
+  div $a0, $t1, 8						# $a0 - Cantidad de bytes para acomodar el max_string.
+										#
+  li $v0, 9								#
+  syscall								#
+										#
+  addi $a0, $v0, 0						# 
+  addi $a1, $0, STRING_MAX				#
+  li $v0, 8								#
+  syscall								# Pido el string.
+										#
+  addi $v0, $a0, 0						# Pongo en $v0 la direccion del string alojado.
+										#
+  jr $ra								#
+#########################################
+# $v0 - Direccion de alojamiento del string
 
-main:
-  la $a1, elemTest
-  la $a0, lista
-  jal listaPush
-  
-  la $a1, elemTest2
-  la $a0, lista
-  jal listaPush
-  
-  li $v0, 10
-  syscall
-  
 
 # Pone un elemento al final de la lista. Recordar que este elemento es una direccion.
-# $a0 - direccion de la lista.
+# $a0 - direccion de la lista. Es decir, direccion donde esta almacenada la direccion del primer elemento.
 # $a1 - direccion del elemento a agregar.
 #########################################
 listaPush:								#
   addi $t0, $a0, 0						#
   addi $t1, $a1, 0						#
-  lw $t2, lista($0)						# $t2 - Direccion del primer elemento.
+  lw $t2, ($a0)							# $t2 - Direccion del primer elemento.
 										#
-  beq $t2, $0, FinRecorrer				# Si $t2 = 0 la lista esta vacia.
+  beq $t2, $0, ListaVacia				# Si $t2 = 0 la lista esta vacia.
 										#
   Recorrer:								# Primero tengo que llegar al ultimo elemento.
     addi $t2, $t2, OFFSET_DIRECCION		#
@@ -49,9 +61,12 @@ listaPush:								#
   li $a0, 8								#
   li $v0, 9								#
   syscall								# Alojo espacio para el nuevo elemento: 8 bytes.
-  addi $t2, $v0, 0						# $t2 - Direccion del nuevo elemento.
 										#
-  sw $v0, 0($t0)						# Hago que el ex-ultimo elemento "apunte" al nuevo.
+  sw $v0, 0($t2)						#
+										#
+  FinVacia:								#
+										#
+  addi $t2, $v0, 0						# $t2 - Direccion del nuevo elemento.
 										#
   sw $0, 0($t2)							# Como el nuevo elemento es el ultimo, la direccion del siguiente es 0.
 										#
@@ -61,5 +76,34 @@ listaPush:								#
   addi $a0, $t0, 0						#
   addi $a1, $t1, 0						#
   jr $ra								#
+										#
+  ListaVacia:							# Caso particular en que tengo que agregar el primer elemento.
+    li $a0, 8							#
+    li $v0, 9							#
+    syscall								# Alojo espacio para el nuevo elemento: 8 bytes.
+										#
+  sw $v0, 0($t0)						#
+										#
+  j FinVacia							#
+										#
 #########################################
 # Esta funcion no retorna nada.
+
+# Saca el primer elemento de la lista, y lo devuelve.
+# $a0 - direccion de la lista. Es decir, direccion donde esta almacenada la direccion del primer elemento.
+#########################################
+listaPop:								#
+  addi $t0, $a0, 0						#
+										#
+  lw $t1, ($t0)							# $t1 - Direccion del primer elemento, el que voy a sacar.
+  lw $t2, ($t1)							# $t2 - Direccion del segundo elemento, el que se volverá primero.
+										#
+  sw $t2, 0($t0)						# Almaceno la direccion del segundo elemento como el nuevo primero.
+										#
+  addi $t1, $t1, OFFSET_DATO			# Me muevo a la segunda componenete del elemento que estoy popeando.
+  lw $v0, ($t1)							# Preparo la direccion que voy a retornar.
+										#
+  addi $a0, $t0, 0						#
+  jr $ra								#
+#########################################
+# $v0 - direccion del string popeado.
